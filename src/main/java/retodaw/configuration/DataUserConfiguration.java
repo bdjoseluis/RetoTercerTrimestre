8fc30ab2257e47880dd9e60abbc2322376b40790
martaclj <104948192+martaclj@users.noreply.github.com>
2025-03-27 22:18:59 +0100

    commit pom y security

---
+++ src/main/java/retodaw/configuration/DataUserConfiguration.java
@@ -0,0 +1,19 @@
+package retodaw.configuration;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class DataUserConfiguration {
+
+}
