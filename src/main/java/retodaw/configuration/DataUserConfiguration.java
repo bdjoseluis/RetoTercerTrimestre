024929169717626e5e586f6bd264f1fc8f951d48
martaclj <104948192+martaclj@users.noreply.github.com>
2025-05-01 09:35:18 +0200

    cambios en securityFilterChain para usar httpBasic(withDetaults()

--- src/main/java/retodaw/configuration/DataUserConfiguration.java
+++ src/main/java/retodaw/configuration/DataUserConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +22,7 @@ public class DataUserConfiguration {
     UserDetailsManager usersCustom(DataSource dataSource) {
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
 
-        // Consultas para usuarios y roles
+        // Consultas para obtener datos del usuario (email = username)
         users.setUsersByUsernameQuery(
             "SELECT email, password, enabled FROM usuarios WHERE email = ?"
         );
@@ -39,25 +41,54 @@ public class DataUserConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    // añadido SuppressWarnings("removal") ya que httpBasic fue suprimido pero aún funciona, para evitar error de deprecated
+    @SuppressWarnings("removal") 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF
+            .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF (para permitir peticiones desde el frontend)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll()  // Permitir acceso sin autenticación a todas las rutas
+            	// Permitir acceso sin autenticación a rutas públicas
+            	// faltaba el /auth antes de /signup!!! y añadimos swagger (q necesita v3/api-docs para funcionar)
+            	.requestMatchers("/auth/signup", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            	.requestMatchers("/signup/**").permitAll() // Permitir acceso sin autenticación a la ruta de registro
+            	
+            	/*Comentado porque era contradictorio
+                //.requestMatchers("/**").permitAll()  // Permitir acceso sin autenticación a todas las rutas
+                */
+            	
+            	// Solo usuarios con rol USUARIO o ADMON pueden acceder a /vacantes/**
                 .requestMatchers("/vacantes/**").hasAnyAuthority("USUARIO", "ADMON")  // Solo usuarios con rol USUARIO o ADMON pueden acceder
+                
+                // Solo usuarios con rol EMPRESA o ADMON pueden acceder a /empresas/**
                 .requestMatchers("/empresas/**").hasAnyAuthority("EMPRESA", "ADMON")  // Solo usuarios con rol EMPRESA o ADMON pueden acceder
+
                 .requestMatchers("/categorias/**").hasAnyAuthority("EMPRESA", "ADMON")
                 .requestMatchers("/solicitudes/**").hasAnyAuthority("EMPRESA", "ADMON")
                 .requestMatchers("/usuarios/solicitudes/:id").hasAnyAuthority("EMPRESA", "ADMON", "Cliente")
-                .requestMatchers("/signup/**").permitAll() // Permitir acceso sin autenticación a la ruta de registro
-                .requestMatchers("/**").hasAuthority("ADMON") // Los usuarios con rol ADMON pueden acceder a todo lo demás
+                
+                /* linea conflictiva: .requestMatchers("/**").hasAuthority("ADMON") // Los usuarios con rol ADMON pueden acceder a todo lo demás
+                (Significa que todo el sistema está restringido a usuarios con el rol ADMON)
+                */
+                
+                .anyRequest().authenticated()
             )
+            
+            /*
             .formLogin(form -> form
                 .defaultSuccessUrl("/login")
                 .failureUrl("/login-error")
                 .permitAll()
             )
+            Esto ha sido comentado porque nos daba error y no permitía hacer las peticiones desde Angular. 
+            En su lugar utilizaremmos httpBasic()
+            */
+            
+            //Activar autenticación básica (envío de usuario-contraseña en cabecera HTTP)
+            // Como está deprecated añadimos withDefaults para completar lo que le falte
+            .httpBasic(withDefaults())
+            
+            //Configuramos por último logout
             .logout(logout -> logout.permitAll());
 
         return http.build();
