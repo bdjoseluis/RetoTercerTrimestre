a25747fe6ab01a1ec47218145c908cb82cf1444c
bdjoseluis <josbotdev@gmail.com>
2025-05-04 10:52:12 +0200

    Update

--- src/test/java/retodaw/RetoDawApplicationTests.java
+++ src/test/java/retodaw/RetoDawApplicationTests.java
@@ -1,13 +1,13 @@
 package retodaw;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RetoDawApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
