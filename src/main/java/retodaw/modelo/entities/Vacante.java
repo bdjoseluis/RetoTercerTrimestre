fc0929decb58afbbb9235ded9bd186243d8fa882
bdjoseluis <josbotdev@gmail.com>
2025-05-05 23:02:13 +0200

    Update

--- src/main/java/retodaw/modelo/entities/Vacante.java
+++ src/main/java/retodaw/modelo/entities/Vacante.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 @Entity
 @Table(name = "vacantes")
 @Schema(description = "vacantes")
@@ -46,4 +43,108 @@ public class Vacante {
     @JoinColumn(name = "id_empresa", referencedColumnName = "id_empresa", nullable = false)
     private Empresa empresa;
 
+    public Vacante(int idVacante, String nombre, String descripcion, Date fecha, double salario, Estatus estatus, Byte destacado, String imagen, String detalles, Categoria categoria, Empresa empresa) {
+        this.idVacante = idVacante;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.salario = salario;
+        this.estatus = estatus;
+        this.destacado = destacado;
+        this.imagen = imagen;
+        this.detalles = detalles;
+        this.categoria = categoria;
+        this.empresa = empresa;
+    }
+
+    public Vacante() {
+    }
+
+    public int getIdVacante() {
+        return idVacante;
+    }
+
+    public void setIdVacante(int idVacante) {
+        this.idVacante = idVacante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Estatus getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(Estatus estatus) {
+        this.estatus = estatus;
+    }
+
+    public Byte getDestacado() {
+        return destacado;
+    }
+
+    public void setDestacado(Byte destacado) {
+        this.destacado = destacado;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
 }
