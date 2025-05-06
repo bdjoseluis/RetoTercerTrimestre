8fd7dce7bcd0c5dfd88af6671fca94be7d7b4043
Patricia <pgutierrezrivas@gmx.es>
2025-05-06 16:38:59 +0200

    add cors configuration and annotations to improve datauserconfiguration file

--- src/main/java/retodaw/dtos/UsuarioRegistroDTO.java
+++ src/main/java/retodaw/dtos/UsuarioRegistroDTO.java
@@ -2,32 +2,13 @@ package retodaw.dtos;
 
 import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @EqualsAndHashCode(of = "email")
 public class UsuarioRegistroDTO {
     private String email;
     private String password;
 
-    public UsuarioRegistroDTO() {
-    }
-
-    public UsuarioRegistroDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
